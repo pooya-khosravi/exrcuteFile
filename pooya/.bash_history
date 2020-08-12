0100051b52162293bc4a1ffa54fafb000e417363
root <root@DESKTOP-OK5N1BM.localdomain>
2020-08-12 18:06:05 +0430

    commited

---
+++ pooya/.bash_history
@@ -0,0 +1,9 @@
+sudo apt update
+lsb_release -a
+sudo apt install build-essential
+مس
+ls
+cd
+ls
+dir
+bash
---
+++ test.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+git init
+git remote add origin https://github.com/pooya-khosravi/exrcuteFile.git
+git add .
+git commit -m "commited"
+git push -f origin master
+
